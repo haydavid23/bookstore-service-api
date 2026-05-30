901ef4df0507f34c953c8a2fe5a8494506650783
Nathaniel <nathern987@gmail.com>
2026-05-30 12:34:57 -0400

    Add database connection and update dependencies

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
 from blueprints.bookdetails import bookdetails_bp
 
+load_dotenv()
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+db = SQLAlchemy(app)
+
 app.register_blueprint(bookdetails_bp)
 
 
